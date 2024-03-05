08335070ce52ad8444a1f99ab0e19da97545db7f
Zhi <yfwz100@yeah.net>
2024-03-06 01:14:56 +0800

    feat: mimic the shadow of gtk scrollbar.

--- data/web/src/main.ts
+++ data/web/src/main.ts
@@ -3,3 +3,15 @@ import { MuyaEditor } from './editor/muya/muya';
 import './main.scss';
 
 self.editor = new MuyaEditor(document.getElementById('editor')!);
+
+document.addEventListener('scroll', () => {
+  const container = document.scrollingElement;
+  if (!container) {
+    return;
+  }
+  if (container.scrollTop > 0) {
+    document.body.classList.add('scrolled');
+  } else {
+    document.body.classList.remove('scrolled');
+  }
+});
